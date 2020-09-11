91b3eb64c7700869b62e059e590781e518547ff6
valentinLampriere <valentin.lampriere1@gmail.com>
2020-09-11 23:31:43 +0200

    Add npc

--- Assets/npcController.cs
+++ Assets/npcController.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class npcController : MonoBehaviour
 {
-
-    float speed = 3f;
+    public GameObject player;
+    float speed = 10f;
     Vector2 movement = new Vector2(1, 0);
     SpriteRenderer sprite;
     Rigidbody2D rb;
+    CircleCollider2D circleCollider;
 
     IEnumerator Start() {
         sprite = gameObject.GetComponent<SpriteRenderer>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        circleCollider = gameObject.GetComponent<CircleCollider2D>();
         yield return Move();
     }
 
     IEnumerator Move() {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        yield return new WaitForSeconds(1);
+        while (!circleCollider.IsTouching(player.GetComponent<CircleCollider2D>())) {
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+            yield return new WaitForSeconds(1);
+        }
     }
 }
