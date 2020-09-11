c5b1b238eba45ce43e1a3ebab3b9967d199b9ae0
valentinLampriere <valentin.lampriere1@gmail.com>
2020-09-11 23:03:11 +0200

    Add character controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float speed = 3f;
+    Vector2 movement;
+    SpriteRenderer sprite;
+    Rigidbody2D rb;
+
+    void Start() {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.x < 0)
+            sprite.flipX = true;
+        else if (movement.x > 0)
+            sprite.flipX = false;
+    }
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/npcController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcController : MonoBehaviour
+{
+
+    float speed = 3f;
+    Vector2 movement = new Vector2(1, 0);
+    SpriteRenderer sprite;
+    Rigidbody2D rb;
+
+    IEnumerator Start() {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        yield return Move();
+    }
+
+    IEnumerator Move() {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        yield return new WaitForSeconds(1);
+    }
+}
