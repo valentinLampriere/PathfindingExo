2f208f3fafaf1ad6ed74966b96c5261b37afb48a
valentinLampriere <valentin.lampriere1@gmail.com>
2020-09-12 09:58:20 +0200

    Create grid class

---
+++ Assets/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid {
+    private int height;
+    private int width;
+    private int[,] gridArray;
+
+    Grid(int w, int h) {
+        height = h;
+        width = w;
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++) 
+            for (int y = 0; y < gridArray.GetLength(1); y++) {
+
+            }
+        }
+    }
+}
