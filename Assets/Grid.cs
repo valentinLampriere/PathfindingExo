93642926b8f096610bcdccbd5fb075abb2f3e88c
valentinLampriere <valentin.lampriere1@gmail.com>
2020-09-12 12:40:50 +0200

    Grid fit with the map

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class Grid {
     private int height;
     private int width;
-    private int[,] gridArray;
+    private float tileSize;
+    private Vector3 origin;
+    private bool[,] gridArray;
 
-    Grid(int w, int h) {
-        height = h;
-        width = w;
-        gridArray = new int[width, height];
+    public Grid(int width, int height, float tileSize, Vector3 origin) {
+        this.height = height;
+        this.width = width;
+        this.tileSize = tileSize;
+        this.origin = origin;
+        gridArray = new bool[width, height];
 
         for (int x = 0; x < gridArray.GetLength(0); x++) {
             for (int y = 0; y < gridArray.GetLength(1); y++) {
-
+                Debug.DrawLine(getPosAtCoord(x, y), getPosAtCoord(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(getPosAtCoord(x, y), getPosAtCoord(x + 1, y), Color.white, 100f);
+                Debug.DrawLine(getPosAtCoord(x + 1, y), getPosAtCoord(x + 1, y + 1), Color.white, 100f);
+                Debug.DrawLine(getPosAtCoord(x, y + 1), getPosAtCoord(x + 1, y + 1), Color.white, 100f);
             }
         }
+        Debug.DrawLine(getPosAtCoord(0, height), getPosAtCoord(width, height), Color.white, 100f);
+        Debug.DrawLine(getPosAtCoord(width, 0), getPosAtCoord(width, height), Color.white, 100f);
+    }
+    private Vector3 getPosAtCoord(int x, int y) {
+        return origin + new Vector3(x, y) * tileSize;
+    }
+
+    private void getCoordAtPosition(Vector3 position, out int x, out int y) {
+        x = Mathf.FloorToInt((position - origin).x / tileSize);
+        y = Mathf.FloorToInt((position - origin).y / tileSize);
+    }
+
+    public void setWall(int x, int y, bool isWall) {
+        gridArray[x, y] = isWall;
+    }
+
+    public bool isWall(int x, int y) {
+        return gridArray[x, y];
     }
 }
---
+++ Assets/GridTesting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTesting : MonoBehaviour {
+    public int width = 10;
+    public int height = 10;
+    void Start() {
+        Grid g = new Grid(width, height, 1f, gameObject.transform.position);
+    }
+}
